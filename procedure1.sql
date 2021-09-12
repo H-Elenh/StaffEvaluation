326b3bda8f82ffed5fef19c01e619f1af51b5429
IoannaAgo <75138406+JoannaAgo@users.noreply.github.com>
2021-09-12 11:25:23 +0300

    Add files via upload

--- procedure1.sql
+++ procedure1.sql
@@ -1,82 +1,49 @@
+DROP PROCEDURE IF EXISTS EmployeeApplications;
 Delimiter $
 CREATE procedure EmployeeApplications(IN empname varchar(25), IN empsurname varchar(35))
 Begin
 
  DECLARE usrapplication VARCHAR(35);
+ declare finishedFlag INT DEFAULT 0;
+ DECLARE tempgrade INT;
  
- DECLARE usreval int(4);
-
- 
- DECLARE empCursor CURSOR FOR 
-	select application from submitapplication
+  DECLARE empCursor CURSOR FOR 
+  
+	select EvId from submitapplication
 	Inner Join employee ON submitapplication.emp_usrname=employee.username
+    INNER JOIN evaluationresult ON evaluationresult.job_id = submitapplication.job_id
 	Inner Join user ON employee.username=user.username
 	Where name=empname AND surname=empsurname;
- 
- 
-  
- declare ecursor Cursor for 
-	 select EvId,eval_username from evaluationresult 
-	 Inner Join employee ON evaluationresult.emp_usrname=employee.username
-	 Inner Join user ON employee.username=user.username
-	 Where name=empname AND surname=empsurname;
- 
- /*Erwthma a*/
-
- open empCursor;
- Begin
-	declare finishedFlag INT;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET finishedFlag=1;
-	SET finishedFlag=0;
- 
-	FETCH empCursor INTO usrapplication;
- 
-	WHILE (finishedFlag=0) DO
-		SELECT application AS ‘Applications’ from submitapplication
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finishedFlag=1;
+    
+    open empCursor;
+    
+    WHILE (finishedFlag=0) DO
+		FETCH empCursor INTO usrapplication;
+        
+        select distinct grade INTO tempgrade from submitapplication
+        INNER JOIN evaluationresult ON evaluationresult.job_id = submitapplication.job_id
+        WHERE evaluationresult.EvId = usrapplication;
+        select tempgrade;
+        IF tempgrade IS NULL THEN
+        select application,'Αξιολόγηση σε εξέλιξη' as status,eval_user.name,eval_user.surname,score_1,score_2,score_3 from submitapplication
 		Inner Join employee ON submitapplication.emp_usrname=employee.username
 		Inner Join user ON employee.username=user.username
-		Where name=empname AND surname=empsurname;
-
+		INNER JOIN evaluationresult ON evaluationresult.job_id = submitapplication.job_id
+		INNER JOIN user eval_user ON evaluationresult.eval_username = eval_user.username
+		INNER JOIN scores ON scores.EvId = evaluationresult.EvId
+        WHERE evaluationresult.EvId = usrapplication;
+        ELSE 
+        select application,grade,eval_user.name,eval_user.surname from submitapplication
+		Inner Join employee ON submitapplication.emp_usrname=employee.username
+		Inner Join user ON employee.username=user.username
+		INNER JOIN evaluationresult ON evaluationresult.job_id = submitapplication.job_id
+		INNER JOIN user eval_user ON evaluationresult.eval_username = eval_user.username
+        WHERE evaluationresult.EvId = usrapplication;
+        END IF;
+        
 	END WHILE;
- END;
 CLOSE empCursor;
-
- 
- /*Erwthma B- evaluations*/
-declare finalgrade int(2);
- select grade into finalgrade from evaluationresult Inner Join employee ON evaluationresult.emp_usrname=employee.username
- Inner Join user ON employee.username=user.username
- Where name=empname AND surname=empsurname;
- 
-open ecursor; 
- 
-Begin
-	declare finishedFlag1 INT;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET finishedFlag1=1;
-	set finishedFlag1=0;
- 
- 
-	FETCH ecursor into usreval;
-   
-	WHILE (finishedFlag1=0) DO
- 
-		if (finalgrade Is not Null) then
- 
-		select EvId AS 'Completed evaluation Id',from evaluationresult 
-		Inner Join employee ON evaluationresult.emp_usrname=employee.username
-		Inner Join user ON employee.username=user.username
-		Where name=empname AND surname=empsurname;
-				
-		else 
-		
-		(select 'Evaluation in progress') UNION
-		(select EvId from evaluationresult Inner Join employee ON evaluationresult.emp_usrname=employee.username
-		Inner Join user ON employee.username=user.username
-		Where name=empname AND surname=empsurname);
-
-		end if;	
-	END WHILE;
- End;
- CLOSE empCursor;
 END$
 DELIMITER ;
